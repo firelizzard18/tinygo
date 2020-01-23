2d4e31da7aa770473af0339f75ef0fa68804abb5
Ethan Reesor <ethan.reesor@gmail.com>
2020-01-23 13:06:28 -0600

    Add type for bitband mapped registers

--- src/runtime/volatile/register_nxpmk66f18.go
+++ src/runtime/volatile/register_nxpmk66f18.go
@@ -20,14 +20,37 @@ func bitbandAddress(reg uintptr, bit uintptr) uintptr {
 	return (reg-registerBase)*ptrBytes*8 + bit*ptrBytes + bitbandBase
 }
 
-// Bit maps bit N of register R to the corresponding bitband address. Bit panics
-// if R is not an AIPS or GPIO register or if N is out of range (greater than
-// the number of bits in a register minus one).
+// Special types that causes loads/stores to be volatile (necessary for
+// memory-mapped registers).
+type BitRegister struct {
+	Reg uint32
+}
+
+// Get returns the of the mapped register bit. It is the volatile equivalent of:
 //
-// go:inline
-func (r *Register8) Bit(bit uintptr) *Register32 {
-	ptr := bitbandAddress(uintptr(unsafe.Pointer(&r.Reg)), bit)
-	return (*Register32)(unsafe.Pointer(ptr))
+//     *r.Reg
+//
+//go:inline
+func (r *BitRegister) Get() bool {
+	return LoadUint32(&r.Reg) != 0
+}
+
+// Set sets the mapped register bit. It is the volatile equivalent of:
+//
+//     *r.Reg = 1
+//
+//go:inline
+func (r *BitRegister) Set() {
+	StoreUint32(&r.Reg, 1)
+}
+
+// Clear clears the mapped register bit. It is the volatile equivalent of:
+//
+//     *r.Reg = 0
+//
+//go:inline
+func (r *BitRegister) Clear() {
+	StoreUint32(&r.Reg, 0)
 }
 
 // Bit maps bit N of register R to the corresponding bitband address. Bit panics
@@ -35,9 +58,9 @@ func (r *Register8) Bit(bit uintptr) *Register32 {
 // the number of bits in a register minus one).
 //
 // go:inline
-func (r *Register16) Bit(bit uintptr) *Register32 {
+func (r *Register8) Bit(bit uintptr) *BitRegister {
 	ptr := bitbandAddress(uintptr(unsafe.Pointer(&r.Reg)), bit)
-	return (*Register32)(unsafe.Pointer(ptr))
+	return (*BitRegister)(unsafe.Pointer(ptr))
 }
 
 // Bit maps bit N of register R to the corresponding bitband address. Bit panics
@@ -45,7 +68,17 @@ func (r *Register16) Bit(bit uintptr) *Register32 {
 // the number of bits in a register minus one).
 //
 // go:inline
-func (r *Register32) Bit(bit uintptr) *Register32 {
+func (r *Register16) Bit(bit uintptr) *BitRegister {
 	ptr := bitbandAddress(uintptr(unsafe.Pointer(&r.Reg)), bit)
-	return (*Register32)(unsafe.Pointer(ptr))
+	return (*BitRegister)(unsafe.Pointer(ptr))
+}
+
+// Bit maps bit N of register R to the corresponding bitband address. Bit panics
+// if R is not an AIPS or GPIO register or if N is out of range (greater than
+// the number of bits in a register minus one).
+//
+// go:inline
+func (r *Register32) Bit(bit uintptr) *BitRegister {
+	ptr := bitbandAddress(uintptr(unsafe.Pointer(&r.Reg)), bit)
+	return (*BitRegister)(unsafe.Pointer(ptr))
 }
