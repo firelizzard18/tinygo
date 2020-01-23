6e596036915a8a54d4da598ab6b2e88b157219f2
Ethan Reesor <ethan.reesor@gmail.com>
2020-01-23 16:08:14 -0600

    Saving work

--- src/machine/machine_nxpmk66f18.go
+++ src/machine/machine_nxpmk66f18.go
@@ -2,28 +2,55 @@
 
 package machine
 
+import (
+	"device/nxp"
+	"runtime/volatile"
+)
+
+const (
+	PortControlRegisterSRE = nxp.PORT_PCR0_SRE
+	PortControlRegisterDSE = nxp.PORT_PCR0_DSE
+	PortControlRegisterODE = nxp.PORT_PCR0_ODE
+)
+
+func PortControlRegisterMUX(v uint8) uint32 {
+	return (uint32(v) << nxp.PORT_PCR0_MUX_Pos) & nxp.PORT_PCR0_MUX_Msk
+}
+
+type pinRegisters struct {
+	Bit  uintptr
+	PCR  *volatile.Register32
+	PDOR *volatile.BitRegister
+	PSOR *volatile.BitRegister
+	PCOR *volatile.BitRegister
+	PTOR *volatile.BitRegister
+	PDIR *volatile.BitRegister
+	PDDR *volatile.BitRegister
+}
+
+// Configure this pin with the given configuration.
+func (p Pin) Configure(config PinConfig) {
+	switch config.Mode {
+	case PinInput:
+		panic("todo")
+
+	case PinOutput:
+		p.registers().PDDR.Set()
+		p.registers().PCR.SetBits(PortControlRegisterSRE | PortControlRegisterDSE | PortControlRegisterMUX(1))
+		p.registers().PCR.ClearBits(PortControlRegisterODE)
+	}
+}
+
 // Set changes the value of the GPIO pin. The pin must be configured as output.
 func (p Pin) Set(value bool) {
-	// if value { // set bits
-	// 	port, mask := p.PortMaskSet()
-	// 	port.Set(mask)
-	// } else { // clear bits
-	// 	port, mask := p.PortMaskClear()
-	// 	port.Set(mask)
-	// }
+	if value {
+		p.registers().PSOR.Set()
+	} else {
+		p.registers().PCOR.Set()
+	}
 }
 
 // Get returns the current value of a GPIO pin.
 func (p Pin) Get() bool {
-	// if p < 8 {
-	// 	val := avr.PIND.Get() & (1 << uint8(p))
-	// 	return (val > 0)
-	// } else if p < 14 {
-	// 	val := avr.PINB.Get() & (1 << uint8(p-8))
-	// 	return (val > 0)
-	// } else {
-	// 	val := avr.PINC.Get() & (1 << uint8(p-14))
-	// 	return (val > 0)
-	// }
-	return false
+	return p.registers().PDIR.Get()
 }
--- src/runtime/runtime_nxp.go
+++ src/runtime/runtime_nxp.go
@@ -4,19 +4,6 @@ package runtime
 
 type timeUnit int64
 
-const tickMicros = 1024 * 32
-
-func ticks() timeUnit {
-	// TODO
-	return 0
-}
-
-const asyncScheduler = false
-
-func sleepTicks(d timeUnit) {
-	// TODO
-}
-
 //go:export Reset_Handler
 func main() {
 	// preinit()
