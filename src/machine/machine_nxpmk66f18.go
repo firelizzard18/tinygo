0ccfeb7fde7cedddd327df0a2b77155e76fae52c
Ethan Reesor <ethan.reesor@gmail.com>
2020-02-18 01:09:16 -0600

    Saving work

--- src/machine/machine_nxpmk66f18.go
+++ src/machine/machine_nxpmk66f18.go
@@ -14,18 +14,13 @@ const (
 )
 
 func PortControlRegisterMUX(v uint8) uint32 {
-	return (uint32(v) << nxp.PORT_PCR0_MUX_Pos) & nxp.PORT_PCR0_MUX_Msk
+	return nxp.PORT_PCR0_MUX(uint32(v))
 }
 
-type pinRegisters struct {
-	Bit  uintptr
+type pinMapping struct {
+	Bit  uint8
 	PCR  *volatile.Register32
-	PDOR *volatile.BitRegister
-	PSOR *volatile.BitRegister
-	PCOR *volatile.BitRegister
-	PTOR *volatile.BitRegister
-	PDIR *volatile.BitRegister
-	PDDR *volatile.BitRegister
+	GPIO *nxp.GPIO_Type
 }
 
 // Configure this pin with the given configuration.
@@ -35,22 +30,56 @@ func (p Pin) Configure(config PinConfig) {
 		panic("todo")
 
 	case PinOutput:
-		p.registers().PDDR.Set()
-		p.registers().PCR.SetBits(PortControlRegisterSRE | PortControlRegisterDSE | PortControlRegisterMUX(1))
-		p.registers().PCR.ClearBits(PortControlRegisterODE)
+		r := p.registers()
+		r.GPIO.PDDR.SetBits(1 << r.Bit)
+		r.PCR.SetBits(PortControlRegisterSRE | PortControlRegisterDSE | PortControlRegisterMUX(1))
+		r.PCR.ClearBits(PortControlRegisterODE)
 	}
 }
 
 // Set changes the value of the GPIO pin. The pin must be configured as output.
 func (p Pin) Set(value bool) {
+	r := p.registers()
 	if value {
-		p.registers().PSOR.Set()
+		r.GPIO.PSOR.Set(1 << r.Bit)
 	} else {
-		p.registers().PCOR.Set()
+		r.GPIO.PCOR.Set(1 << r.Bit)
 	}
 }
 
 // Get returns the current value of a GPIO pin.
 func (p Pin) Get() bool {
-	return p.registers().PDIR.Get()
+	r := p.registers()
+	return r.GPIO.PDIR.HasBits(1 << r.Bit)
 }
+
+func (p Pin) Control() *volatile.Register32 {
+	return p.registers().PCR
+}
+
+type FastPin struct {
+	PDOR *volatile.BitRegister
+	PSOR *volatile.BitRegister
+	PCOR *volatile.BitRegister
+	PTOR *volatile.BitRegister
+	PDIR *volatile.BitRegister
+	PDDR *volatile.BitRegister
+}
+
+func (p Pin) Fast() FastPin {
+	r := p.registers()
+	return FastPin{
+		PDOR: r.GPIO.PDOR.Bit(r.Bit),
+		PSOR: r.GPIO.PSOR.Bit(r.Bit),
+		PCOR: r.GPIO.PCOR.Bit(r.Bit),
+		PTOR: r.GPIO.PTOR.Bit(r.Bit),
+		PDIR: r.GPIO.PDIR.Bit(r.Bit),
+		PDDR: r.GPIO.PDDR.Bit(r.Bit),
+	}
+}
+
+func (p FastPin) Set()         { p.PSOR.Set(true) }
+func (p FastPin) Clear()       { p.PCOR.Set(true) }
+func (p FastPin) Toggle()      { p.PTOR.Set(true) }
+func (p FastPin) Write(v bool) { p.PDOR.Set(v) }
+func (p FastPin) Read() bool   { return p.PDIR.Get() }
